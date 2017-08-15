16892910f9bd0beb5109d4a4606de534ad4b8b7a
niladrikar0306 <niladri.kar0306@gmail.com>
2017-08-16 00:19:11 +0530

    unknown

---
+++ algos1/src/abc/Test4.java
@@ -0,0 +1,12 @@
+package abc;
+public class Test4 {
+  public static void main(String[] args){
+    int[] a = {1};
+    Test t = new Test();
+    t.increment(a);
+    System.out.println(a[a.length - 1]);
+  }
+  void increment(int[] i){
+     i[i.length - 1]++; 
+  }
+}
